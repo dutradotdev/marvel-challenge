8b727ce0c28fa10872b6c296f3eb5f2f091f5329
Lucas Dutra <dtrxxl@outlook.com>
2021-09-28 14:18:42 -0300

    feat: persist gate to persist info about fav heroes

--- App.tsx
+++ App.tsx
@@ -1,22 +1,21 @@
 /* global __DEV__ */
 import React from 'react'
-import { LogBox } from 'react-native'
 import 'react-native-gesture-handler'
 import { Provider } from 'react-redux'
-import store from '@marvel/redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from '@marvel/redux/store'
 import Router from '@marvel/router'
 
 if (__DEV__) {
-  import('@marvel/config/ReactotronConfig').then(() => console.log('Reactotron Configured'))
+  import('@marvel/config/ReactotronConfig')
 }
-
-// ignore warnings from react-native-avataaars
-LogBox.ignoreAllLogs()
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Router />
+      <PersistGate loading={null} persistor={persistor}>
+        <Router />
+      </PersistGate>
     </Provider>
   )
 }
